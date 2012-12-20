dba25ef4e53d37620a7a86cd3e369782f88dbce8
Baptiste LEPERS <baptiste.lepers@inria.fr>
2012-12-20 14:40:14 +0100

    Correct plots for DRAM locality and total DRAM accesses to all nodes

--- File/MiniProf/Results/DRAM.pm
+++ File/MiniProf/Results/DRAM.pm
@@ -15,11 +15,6 @@ use File::MiniProf;
 #[ 'CPU_DRAM_Node0', 'CPU_DRAM_Node1', 'CPU_DRAM_Node2', 'CPU_DRAM_Node3' ],
 sub local_dram_usage {
    my ( $self, $info, $parse_options, $opt ) = @_;
-   my $plot;
-
-   if ( $opt->{gnuplot} ) {
-      $plot = File::MiniProf::Results::Plot::get_plot( $info, $parse_options, $opt, $parse_options->{ $info->{name} }->{name} );
-   }
 
    my $global_sum_local;
    my $global_sum_all;
@@ -81,27 +76,52 @@ sub local_dram_usage {
 
 
    if ( $opt->{gnuplot} ) {
-#      my @plota;
-#      
-#      for my $die ( 0 .. 3 ) {
-#         my $local_dram = &{$local_dram_fun}($die);
-#         my @vals = ();
-#         for ( my $i = 0 ; $i < scalar( @{ $self->{miniprof}->{raw}->{$die}->{ $events[0] }->{val} } ) ; $i++ ) {
-#            my $val_0 = $self->{miniprof}->{raw}->{$die}->{ $events[$local_dram] }->{val}->[$i];    # LOCAL
-#            my $val_1 = 0;
-#            for my $k ( 0 .. 3 ) {
-#               if ( $k != $local_dram ) {
-#                  $val_1 += $self->{miniprof}->{raw}->{$die}->{ $events[$k] }->{val}->[$i];                   # LOCAL
-#               }
-#            }
-#            my $avg = ( $val_1 ) ? ( $val_0 / $val_1 ) : 0;
-#            push( @vals, $avg );
-#         }
-#         push( @plota, \@vals );     
-#      }
-#      
-#      $plot->gnuplot_set_plot_titles( map( "Die $_", ( 0 .. 3 ) ) );
-#      $plot->gnuplot_plot_xy( $self->{miniprof}->{raw}->{0}->{ $events[0] }->{time}, @plota ); 
+      my $plot = File::MiniProf::Results::Plot::get_plot( $info, $parse_options, $opt, $parse_options->{ $info->{name} }->{name} );
+      my @plota;
+      
+      my $random_core = ( keys %{ $self->{miniprof}->{raw} } )[0];
+      for my $k ( 0 .. 3 ) {
+         my @vals = ();
+         for ( my $i = 0 ; $i < scalar( @{ $self->{miniprof}->{raw}->{$random_core}->{ $events[0] }->{val} } ) ; $i++ ) {
+            my $val_local = 0;
+            my $val_dist = 0;
+            for my $core ( keys %{ $self->{miniprof}->{raw} } ) {
+               my $local_dram = File::MiniProf::_local_dram_fun($self, $core, $opt->{local_dram_fun});
+               if($k == $local_dram) {
+                  $val_local += $self->{miniprof}->{raw}->{$core}->{ $events[$k] }->{val}->[$i];                
+               } else {
+                  $val_dist += $self->{miniprof}->{raw}->{$core}->{ $events[$k] }->{val}->[$i];                
+               }
+            }
+            my $avg = ( $val_local + $val_dist ) ? ( $val_local / ($val_local + $val_dist) ) : 0;
+            push( @vals, $avg );
+         }
+         push( @plota, \@vals );     
+      }
+
+      $plot->gnuplot_set_plot_titles( map( "Die $_", ( 0 .. 3 ) ) );
+      $plot->gnuplot_plot_xy( $self->{miniprof}->{raw}->{0}->{ $events[0] }->{time}, @plota ); 
+   }
+
+   if ( $opt->{gnuplot} ) {
+      my $plot = File::MiniProf::Results::Plot::get_plot( $info, $parse_options, $opt, 'DRAM Accesses to Nodes');
+      my @plota;
+      
+      my $random_core = ( keys %{ $self->{miniprof}->{raw} } )[0];
+      for my $k ( 0 .. 3 ) {
+         my @vals = ();
+         for ( my $i = 0 ; $i < scalar( @{ $self->{miniprof}->{raw}->{$random_core}->{ $events[0] }->{val} } ) ; $i++ ) {
+            my $val = 0;
+            for my $core ( keys %{ $self->{miniprof}->{raw} } ) {
+               $val += $self->{miniprof}->{raw}->{$core}->{ $events[$k] }->{val}->[$i];                
+            }
+            push( @vals, $val );
+         }
+         push( @plota, \@vals );     
+      }
+      
+      $plot->gnuplot_set_plot_titles( map( "Die $_", ( 0 .. 3 ) ) );
+      $plot->gnuplot_plot_xy( $self->{miniprof}->{raw}->{0}->{ $events[0] }->{time}, @plota ); 
    }
 }
 
